48ecaa9293edaf4938ec3e502f6c6349f0ae9c6e
Your Name <your_email@example.com>
2026-04-11 16:28:46 +0530

    UC4: OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,38 +1,25 @@
 /**
  * OOPSBannerApp
- * UC3: Print OOPS banner using String.join()
+ * UC4: OOPS banner using Array and Loop
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String line1 = String.join(" ",
-                " OOO ", " OOO ", " PPPP ", " PPPP ", " SSSS ");
+        // Store all lines in array
+        String[] banner = {
+                String.join(" ", " OOO ", " OOO ", " PPPP ", " PPPP ", " SSSS "),
+                String.join(" ", "O   O", "O   O", "P   P", "P   P", "S     "),
+                String.join(" ", "O   O", "O   O", "P   P", "P   P", "S     "),
+                String.join(" ", "O   O", "O   O", "PPPP ", "PPPP ", " SSS  "),
+                String.join(" ", "O   O", "O   O", "P    ", "P    ", "    S "),
+                String.join(" ", "O   O", "O   O", "P    ", "P    ", "    S "),
+                String.join(" ", " OOO ", " OOO ", "P    ", "P    ", " SSSS ")
+        };
 
-        String line2 = String.join(" ",
-                "O   O", "O   O", "P   P", "P   P", "S     ");
-
-        String line3 = String.join(" ",
-                "O   O", "O   O", "P   P", "P   P", "S     ");
-
-        String line4 = String.join(" ",
-                "O   O", "O   O", "PPPP ", "PPPP ", " SSS  ");
-
-        String line5 = String.join(" ",
-                "O   O", "O   O", "P    ", "P    ", "    S ");
-
-        String line6 = String.join(" ",
-                "O   O", "O   O", "P    ", "P    ", "    S ");
-
-        String line7 = String.join(" ",
-                " OOO ", " OOO ", "P    ", "P    ", " SSSS ");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        // Loop to print
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
