6378000848d200b93aa3acbcadf4e0efd0995c05
Your Name <your_email@example.com>
2026-04-11 15:46:49 +0530

    UC2: OOPS ASCII banner implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,20 @@
+/**
+ * OOPSBannerApp
+ * UC2: Print OOPS in ASCII banner format
+ *
+ * @author YourName
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  OOO   OOO   PPPP   PPPP   SSSS ");
+        System.out.println(" O   O O   O  P   P  P   P  S    ");
+        System.out.println(" O   O O   O  P   P  P   P  S    ");
+        System.out.println(" O   O O   O  PPPP   PPPP    SSS ");
+        System.out.println(" O   O O   O  P      P          S ");
+        System.out.println(" O   O O   O  P      P          S ");
+        System.out.println("  OOO   OOO   P      P      SSSS ");
     }
 }
