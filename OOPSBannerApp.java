18327e0f57daac2eb1d79e15df2b9bf8df5ac2fc
Your Name <your_email@example.com>
2026-04-11 16:25:05 +0530

    UC3: OOPS banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,38 @@
+/**
+ * OOPSBannerApp
+ * UC3: Print OOPS banner using String.join()
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String line1 = String.join(" ",
+                " OOO ", " OOO ", " PPPP ", " PPPP ", " SSSS ");
+
+        String line2 = String.join(" ",
+                "O   O", "O   O", "P   P", "P   P", "S     ");
+
+        String line3 = String.join(" ",
+                "O   O", "O   O", "P   P", "P   P", "S     ");
+
+        String line4 = String.join(" ",
+                "O   O", "O   O", "PPPP ", "PPPP ", " SSS  ");
+
+        String line5 = String.join(" ",
+                "O   O", "O   O", "P    ", "P    ", "    S ");
+
+        String line6 = String.join(" ",
+                "O   O", "O   O", "P    ", "P    ", "    S ");
+
+        String line7 = String.join(" ",
+                " OOO ", " OOO ", "P    ", "P    ", " SSSS ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
     }
 }
